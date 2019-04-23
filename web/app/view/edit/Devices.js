e442d8a4ba1557fc79007daaaf1df8bcc1ea28af
bolt33141 <B0ltSharedProductionHard>
2019-04-23 15:05:03 -0400

    fix null in device manager, add back filters on device

--- web/app/view/edit/Devices.js
+++ web/app/view/edit/Devices.js
@@ -104,19 +104,19 @@ Ext.define('Traccar.view.edit.Devices', {
         }, {
             text: Strings.deviceIdentifier,
             dataIndex: 'uniqueId',
-            hidden: true
+            filter: 'string'
         }, {
             text: Strings.sharedPhone,
             dataIndex: 'phone',
-            hidden: true
+            filter: 'string'
         }, {
             text: Strings.deviceModel,
             dataIndex: 'model',
-            hidden: true
+            filter: 'string'
         }, {
             text: Strings.deviceContact,
             dataIndex: 'contact',
-            hidden: true
+            filter: 'string'
         }, {
             text: Strings.groupDialog,
             dataIndex: 'groupId',
